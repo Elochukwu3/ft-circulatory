48ffd55aaefdf25d35ef17cea72b69ba06ef66a0
Elochukwu3 <103644673+Elochukwu3@users.noreply.github.com>
2022-04-29 12:56:49 -0500

    final circulatory

---
+++ app.js
@@ -0,0 +1,11 @@
+let numb= document.querySelector(".numb");
+
+let counter=0;
+setInterval(() => {
+ if (counter == 100) {
+     clearInterval();
+ } else {
+    counter+=1;
+    numb.textContent= counter + "%" ;
+ }
+}, 100);
